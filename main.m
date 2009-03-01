3eae7951da721205d66447add596cfdcbbf37039
Wesley Moore <wez@wezm.net>
2009-03-01 14:58:33 +1100

    Initial Commmit

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Allocator
+//
+//  Created by Wesley Moore on 1/03/09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
